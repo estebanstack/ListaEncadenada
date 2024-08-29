b223947ba722a4742d4ada7e81bfc0eca44b2de6
estebanstack <130319203+estebanstack@users.noreply.github.com>
2024-08-29 10:24:34 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+package ListaEncadenada;
+public class Main {
+    public static void main(String[] args) {
+        ListaEncadenada<Integer> lista = new ListaEncadenada<Integer>();
+
+        lista.agregarFinal(10);
+        lista.mostrar();
+
+    }
+}
